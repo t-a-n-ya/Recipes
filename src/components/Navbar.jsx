9bbc6228059e13f765b3a4a99dad40d963415a2f
Tanya <bhagattanya4@gmail.com>
2023-02-15 00:28:13 +0530

    updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,22 @@ import React from 'react'
 function Navbar() {
   return (
     <>
-        <header id="header" class="fixed-top d-flex align-items-cente">
-    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
-    
-      <h1 class="logo me-auto me-lg-0"><a href="index.html" class="a">MyCookBook</a></h1>
+      <header id="header" class="fixed-top d-flex align-items-center">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
 
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#about">About</a></li>
-          <li><a class="nav-link scrollto" href="#menu">Categories</a></li>
-          <li><a class="nav-link scrollto" href="#specials">Specials</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+          <h1 class="logo me-auto me-lg-0"><a href="#" class="a">MyCookBook</a></h1>
 
-    </div>
-  </header>
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              <li><a class="nav-link scrollto active" href="#about">About</a></li>
+              <li><a class="nav-link scrollto" href="#menu">Categories</a></li>
+              <li><a class="nav-link scrollto" href="#specials">Specials</a></li>
+              <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+            </ul>
+          </nav>
+
+        </div>
+      </header>
     </>
   )
 }
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -7,13 +7,42 @@ function Recipe() {
   return (
     <>
       <Navbar />
-      <div className='a'>
+      <div>
+      <div className='a-recipe'>
         <div className='b'>
           <div className='c'>
             <img src={pizza} width='450px' height='600px'/>
-            <h2>ffffffffffffffffffffffffffffffffffffffffff</h2>
+            <div className='recipe-info'>
+              <h2>Recipe name</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                quis sed do eiusmod tempor incididunt ut labore et nostrud ut aliquip ex ea commodo consequate</p>
+              <div style={{display:"flex", justifyContent:"space-between",flexDirection:"column"}}>
+                <strong>prep time : 5 min</strong>
+                <strong>cooking time : 30 min</strong>
+                <strong>serving : 4</strong>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <div class='container'>
+        <h1>Ingredients</h1>
+        <p>hykhhhlhlu</p>
+        <p>hykhhhlhlu</p>
+        <p>hykhhhlhlu</p>
+        <p>hykhhhlhlu</p>
+        <p>hykhhhlhlu</p>
+        <h1>Directions</h1>
+        <strong>step 1</strong>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            <strong>step 2</strong>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+            <strong>step 3</strong>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+      </div>
       </div>
     </>
   )
